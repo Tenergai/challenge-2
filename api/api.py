e7ce46b6273740a41bbb11536cc5276640be569d
rafaeldpsilva <1190979@isep.ipp.pt>
2023-01-07 16:50:07 +0000

    dasboard

---
+++ api/api.py
@@ -0,0 +1,22 @@
+import sys
+from flask import Flask, request, jsonify
+
+sys.path.append('.')
+from Forecast import Forecast
+
+app = Flask(__name__)
+forecast = Forecast()
+
+@app.route('/', methods=['GET'])
+def home():
+    return jsonify({'hello':'helloooo world'})
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    request_json = request.get_json()
+    data = request_json['array']
+    prediction = forecast.predict(data)[0]
+    return jsonify({'result':prediction})
+
+if __name__ == '__main__':
+    app.run(port=5002)
