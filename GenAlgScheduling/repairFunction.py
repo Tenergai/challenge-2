8954ca65a10bc3ccba2bc4a716ef5d87adf479c7
rafaelbarbarroxa <rafaelbarbarroxa@gmail.com>
2023-01-11 17:38:53 +0000

    Consecutive hours check

--- GenAlgScheduling/repairFunction.py
+++ GenAlgScheduling/repairFunction.py
@@ -34,7 +34,8 @@ def repairFunction(m):
     for i, row in enumerate(matrixToFix):
         # check hours on
         current_device = devices[i]
-        print("Checking Device: ", i, current_device["name"])
+        print("##########")
+        print("Checking Device: ", i, " - ", current_device["name"])
         hours_on = sum(row)
         print("Necessary Hours: ", current_device["hoursOn"], "Actual Hours Turned On: ", hours_on)
         if current_device["hoursOn"] != hours_on:
@@ -42,9 +43,47 @@ def repairFunction(m):
             # break
         else:
             print("Valid Matrix - Necessary Hours")
-
+        print("##########")
+        print("Consecutives: ")
+        print(row)
         # check consecutive hours
+        consecutives_max = 0;
+        consecutives_min = 99;
+        consecutives_counter = 0;
+        for y in range(len(row)):
+            if row[y] == 1:
+                consecutives_counter += 1;
+                if y == len(row) - 1:
+                    print("Last iteration")
+                    # consecutives_total.append(consecutives_counter)
+                    if consecutives_counter > consecutives_max:
+                        consecutives_max = consecutives_counter
+                    elif consecutives_counter < consecutives_min and consecutives_counter != 0:
+                        consecutives_min = consecutives_counter
+                    consecutives_counter = 0
+            elif row[y] == 0:
+                # consecutives_total.append(consecutives_counter)
+                if consecutives_counter > consecutives_max:
+                    consecutives_max = consecutives_counter
+                elif consecutives_counter < consecutives_min and consecutives_counter != 0:
+                    consecutives_min = consecutives_counter
+                consecutives_counter = 0
+        #ajeitar isto
+        if current_device["consecutiveHours"] != consecutives_max and current_device[
+            "consecutiveHours"] != consecutives_min:
+            print("Invalid consecutive: ", "Consecutive Hours: ", current_device["consecutiveHours"], "Max: ",
+                  consecutives_max, "Min: ", consecutives_min)
+        else:
+            print("Valid consecutive: ", "Consecutive Hours: ", current_device["consecutiveHours"], "Max: ",
+                  consecutives_max, "Min: ", consecutives_min)
+        # if row[y] == row[y + 1] and row[y] == 1:
+        #   consecutives.append(y)
+        #  print("Consecutive 1's found")
+        # elif row[y] == row[y - 1] and row[y] == 1:
+        #   consecutives.append(y)
 
+        # print(consecutives)
+        print("##########")
         # check possible hours
         hours_device_was_on = np.where(row == 1)
         print("Possible Hours: ", current_device["possibleHours"], "Hours it was on: ", hours_device_was_on[0])
@@ -53,17 +92,13 @@ def repairFunction(m):
             print("Invalid Matrix - Possible Hours")
         else:
             print("Valid Matrix - Possible Hours")
-
+        print("##########")
         # check precedences
-        print("Current Device: ", i, current_device["name"])
-        print("##################")
+        # print("Current Device: ", i, current_device["name"])
         preceded_by = list(np.where(precedences[i] == 1))
         print(preceded_by[0])
 
-        print("##################")
-            # if 0 - 1, then preceding device hr before must be 1
-
-
+    # if 0 - 1, then preceding device hr before must be 1
 
     # for column in matrix.T:
     # print(column)
