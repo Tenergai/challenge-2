54d656e1786c0f91db994ec4615ebaf8af5f3c71
LeticiaGomes1181466 <94696179+LeticiaGomes1181466@users.noreply.github.com>
2023-01-12 22:43:12 +0000

    genetic algorithm updated

--- GenAlgScheduling/objectiveFunction.py
+++ GenAlgScheduling/objectiveFunction.py
@@ -1,51 +1,32 @@
 import numpy as np
+from deviceSpecification import getDevices
+from generateMatrix import generateMatrix
 from utils import getPricesForToday,getDailyGeneration
 price = getPricesForToday()
-#np.random.rand(24)
-# print(price)
 
-generation = np.random.randint(10, size=24)#getDailyGeneration()
-# print(generation)
-
-# devices = np.random.randint(2, size=(10, 24))
-# print('check',devices)
+generation = getDailyGeneration()
 
 consumption = np.random.rand(10, 24)
 
 profit = np.zeros(24)
 
 
-# print(consumption)
-
 def multiply_arrays(a, b):
     return [x * y for x, y in zip(a, b)]
 
 
-def calculateConsumption(devices,hour):
-    devicesOnThatHour = devices[:, hour]
+def calculateConsumption(devicesHourly,hour):
+    devicesOnThatHour = devicesHourly[:, hour]
     consumptionOnThatHour = consumption[:, hour]
-    # print(devicesOnThatHour)
-    # print(consumptionOnThatHour)
     result = multiply_arrays(devicesOnThatHour, consumptionOnThatHour)
-    # print("result")
-    # print(result)
-    # print("#################")
     return sum(result)
 
 
-def objectiveFunction(profit,ind,devices):
+def objectiveFunction(profit,devicesHourly):
     profit = np.zeros(24)
     for h in range(24):
-        consumptionHour = calculateConsumption(devices,h)
-        # print("cosumptionHour")
-        # print(consumptionHour)
+        consumptionHour = calculateConsumption(devicesHourly,h)
         profit[h] = price[h] * (generation[h] * consumptionHour)
     totalProfit = sum(profit)
     return totalProfit
 
-
-# p, total = objectiveFunction()
-# print(p)
-# print(total)
-
-# calculateConsumption(0)
