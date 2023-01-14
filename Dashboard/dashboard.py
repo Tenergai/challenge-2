a86ef41130ea52a6671f98cc4ab73c75b7a34eed
rafaeldpsilva <1190979@isep.ipp.pt>
2023-01-14 17:29:32 +0000

    1hour devices

--- Dashboard/dashboard.py
+++ Dashboard/dashboard.py
@@ -21,17 +21,36 @@ def fetch_data(data):
 best_individual, device_names, matriz, day = ga()
 
 def build_matrix_graph(matrix):
+    matrix = extend_one_hour(matrix)
     matrix = np.transpose(matrix)
     matrix = matrix.tolist()
+    matrix = set_null_values_nan(matrix)
+    matrix = pd.DataFrame(matrix, columns=device_names)
+    #print(matrix)
+    sl.line_chart(matrix)    
+
+def extend_one_hour(matrix):
+    for device in range(len(matrix)):
+        for hour in range(len(matrix[device])):
+            if hour < 23:
+                if matrix[device][hour] == 1 and matrix[device][hour+1] == 0:
+                    matrix[device][hour+1] = -1
+
+    for device in range(len(matrix)):
+        for hour in range(len(matrix[device])):
+            if matrix[device][hour] == -1:
+                matrix[device][hour] = 1
+    return matrix
+
+def set_null_values_nan(matrix):
     for i, hour in enumerate(matrix):
         for j,device in enumerate(hour):
             if device == 1:
                 matrix[i][j] = device * (j+1)
+                
             else:
                 matrix[i][j] = np.nan
-    matrix = pd.DataFrame(matrix, columns=device_names)
-    sl.line_chart(matrix)    
-
+    return matrix
 def update_graph(day):
     #df = fetch_data(day)
     # Create the figure
