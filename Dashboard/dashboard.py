a3248b28195a3d0f051305eaf55e93b9fa53a4af
rafaeldpsilva <1190979@isep.ipp.pt>
2023-01-14 16:26:34 +0000

    dashboard

--- Dashboard/dashboard.py
+++ Dashboard/dashboard.py
@@ -3,7 +3,9 @@ import plotly.express as px
 import requests
 import pandas as pd
 import numpy as np
-
+import sys
+sys.path.append('.')
+from GenAlgScheduling.ga import ga
 
 # Function to fetch the data from the API
 def fetch_data(data):
@@ -16,7 +18,7 @@ def fetch_data(data):
     df = pd.DataFrame(data)
     return df
 
-mock = np.random.randint(2, size=(5,24))
+best_individual, matriz, day = ga()
 
 def build_matrix_graph(matrix):
     matrix = np.transpose(matrix)
@@ -30,32 +32,10 @@ def build_matrix_graph(matrix):
     matrix = pd.DataFrame(matrix)
     sl.line_chart(matrix)    
 
-def update_graph():
-    day = [[0,8,6,1023,109,1,4,81,0,0,0],
-    [1,8,5,1023,5,1,6,81,0,0,0],
-    [2,7,5,1023,5,4,8,82,0,0,0],
-    [3,7,4,1023,41,8,11,83,0,0,0],
-    [4,6,4,1023,349,1,11,85,0,0,0],
-    [5,6,3,1024,342,0,9,86,0,0,0],
-    [6,6,3,1025,51,1,4,86,0,0,0],
-    [8,8,5,1026,59,4,8,79,0,0,116],
-    [9,9,5,1027,56,14,16,76,0,0,313],
-    [10,10,6,1027,71,4,14,72,0,0,534],
-    [11,13,5,1027,65,8,20,61,0,0,694],
-    [12,14,5,1027,346,25,27,58,0,0,728],
-    [13,14,8,1027,323,14,22,68,0,0,932],
-    [14,14,7,1027,323,14,24,65,0,0,638],
-    [15,14,8,1027,284,29,29,69,0,0,675],
-    [16,14,8,1027,338,19,32,69,0,0,564],
-    [17,14,9,1027,333,17,20,71,0,0,309],
-    [18,12,9,1027,349,9,24,79,0,0,67],
-    [19,12,9,1027,155,11,19,83,0,0,11],
-    [21,12,10,1028,320,3,14,87,0,0,0],
-    [22,12,10,1029,305,1,6,87,0,0,0],
-    [23,12,10,1029,342,3,6,87,0,0,0]]
-    df = fetch_data(day)
+def update_graph(day):
+    #df = fetch_data(day)
     # Create the figure
-    sl.line_chart(df)
+    sl.line_chart(day)
 
 
 sl.write("""
@@ -73,9 +53,9 @@ sl.write("""
 17, 1152,
 18, 418,
 19, 73, """
-update_graph()
+update_graph(day)
 
 sl.write("""
 ## Device Scheduling
 """)
-build_matrix_graph(mock)
+build_matrix_graph(matriz)
